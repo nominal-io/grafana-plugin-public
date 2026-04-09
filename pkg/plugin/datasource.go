00337d3b6e23e79ea25eddfbc3649bc956dc3c36
Duane Eldridge <duane.eldridge@nominal.io>
2026-04-09 15:05:12 -0500

    fix: update error message for supported aggregations

--- pkg/plugin/datasource.go
+++ pkg/plugin/datasource.go
@@ -427,7 +427,7 @@ func (d *Datasource) QueryData(ctx context.Context, req *backend.QueryDataReques
 			} else if deduped, badAgg := validateAndDedup(qm.Aggregations); badAgg != "" {
 				response.Responses[q.RefID] = backend.ErrDataResponse(
 					backend.StatusBadRequest,
-					fmt.Sprintf("unsupported aggregation %q; valid options are MEAN, MIN, MAX", badAgg),
+					fmt.Sprintf("unsupported aggregation %q; valid options are MEAN, MIN, MAX, COUNT, VARIANCE", badAgg),
 				)
 				continue
 			} else {
