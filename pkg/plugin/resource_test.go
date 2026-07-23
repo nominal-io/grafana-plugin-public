0e6f30f1fe7f64739e07fc5c874b2d05aee8cc06
Duane Eldridge <duane.eldridge@nominal.io>
2026-07-22 18:54:46 -0700

    test: move shared datasource mock to helpers

--- pkg/plugin/resource_test.go
+++ pkg/plugin/resource_test.go
@@ -10,10 +10,7 @@ import (
 	"testing"
 
 	"github.com/grafana/grafana-plugin-sdk-go/backend"
-	"github.com/nominal-io/nominal-api-go/api/rids"
 	authapi "github.com/nominal-io/nominal-api-go/authentication/api"
-	datasourceapi "github.com/nominal-io/nominal-api-go/datasource/api"
-	"github.com/nominal-io/nominal-api-go/io/nominal/api"
 	datasourceservice "github.com/nominal-io/nominal-api-go/scout/datasource"
 	"github.com/palantir/pkg/bearertoken"
 )
@@ -88,65 +85,8 @@ func (m *mockAuthService) ResetMyCoachmarkDismissal(ctx context.Context, authHea
 	return nil
 }
 
-// mockDatasourceService implements datasourceservice.DataSourceServiceClient for testing
-type mockDatasourceService struct {
-	searchChannelsResponse datasourceapi.SearchChannelsResponse
-	searchChannelsError    error
-	searchChannelsRequest  datasourceapi.SearchChannelsRequest
-	searchChannelsCalls    int
-	// searchChannelsFunc, when non-nil, overrides searchChannelsResponse/searchChannelsError.
-	// This allows tests to return different responses on successive calls (e.g. pagination).
-	searchChannelsFunc func(ctx context.Context, authHeader bearertoken.Token, req datasourceapi.SearchChannelsRequest) (datasourceapi.SearchChannelsResponse, error)
-}
-
-func (m *mockDatasourceService) SearchChannels(ctx context.Context, authHeader bearertoken.Token, queryArg datasourceapi.SearchChannelsRequest) (datasourceapi.SearchChannelsResponse, error) {
-	m.searchChannelsCalls++
-	m.searchChannelsRequest = queryArg
-	if m.searchChannelsFunc != nil {
-		return m.searchChannelsFunc(ctx, authHeader, queryArg)
-	}
-	return m.searchChannelsResponse, m.searchChannelsError
-}
-
-func (m *mockDatasourceService) SearchFilteredChannels(ctx context.Context, authHeader bearertoken.Token, queryArg datasourceapi.SearchFilteredChannelsRequest) (datasourceapi.SearchFilteredChannelsResponse, error) {
-	return datasourceapi.SearchFilteredChannelsResponse{}, nil
-}
-
-func (m *mockDatasourceService) SearchHierarchicalChannels(ctx context.Context, authHeader bearertoken.Token, queryArg datasourceapi.SearchHierarchicalChannelsRequest) (datasourceapi.SearchHierarchicalChannelsResponse, error) {
-	return datasourceapi.SearchHierarchicalChannelsResponse{}, nil
-}
-
-func (m *mockDatasourceService) IndexChannelPrefixTree(ctx context.Context, authHeader bearertoken.Token, requestArg datasourceapi.IndexChannelPrefixTreeRequest) (datasourceapi.ChannelPrefixTree, error) {
-	return datasourceapi.ChannelPrefixTree{}, nil
-}
-
-func (m *mockDatasourceService) BatchGetChannelPrefixTrees(ctx context.Context, authHeader bearertoken.Token, requestArg datasourceapi.BatchGetChannelPrefixTreeRequest) (datasourceapi.BatchGetChannelPrefixTreeResponse, error) {
-	return datasourceapi.BatchGetChannelPrefixTreeResponse{}, nil
-}
-
-func (m *mockDatasourceService) GetAvailableTagsForChannel(ctx context.Context, authHeader bearertoken.Token, requestArg datasourceapi.GetAvailableTagsForChannelRequest) (datasourceapi.GetAvailableTagsForChannelResponse, error) {
-	return datasourceapi.GetAvailableTagsForChannelResponse{}, nil
-}
-
-func (m *mockDatasourceService) GetDataScopeBounds(ctx context.Context, authHeader bearertoken.Token, requestArg datasourceapi.BatchGetDataScopeBoundsRequest) (datasourceapi.BatchGetDataScopeBoundsResponse, error) {
-	return datasourceapi.BatchGetDataScopeBoundsResponse{}, nil
-}
-
-func (m *mockDatasourceService) GetTagValuesForDataSource(ctx context.Context, authHeader bearertoken.Token, dataSourceRidArg rids.DataSourceRid, requestArg datasourceapi.GetTagValuesForDataSourceRequest) (map[api.TagName][]api.TagValue, error) {
-	return nil, nil
-}
-
-func (m *mockDatasourceService) GetAvailableTagKeys(ctx context.Context, authHeader bearertoken.Token, dataSourceRidArg rids.DataSourceRid, requestArg datasourceapi.GetAvailableTagKeysRequest) (datasourceapi.GetAvailableTagKeysResponse, error) {
-	return datasourceapi.GetAvailableTagKeysResponse{}, nil
-}
-
-func (m *mockDatasourceService) GetAvailableTagValues(ctx context.Context, authHeader bearertoken.Token, dataSourceRidArg rids.DataSourceRid, requestArg datasourceapi.GetAvailableTagValuesRequest) (datasourceapi.GetAvailableTagValuesResponse, error) {
-	return datasourceapi.GetAvailableTagValuesResponse{}, nil
-}
-
 // Verify mock types implement their interfaces at compile time
 var _ authapi.AuthenticationServiceV2Client = (*mockAuthService)(nil)
-var _ datasourceservice.DataSourceServiceClient = (*mockDatasourceService)(nil)
 
 // callResourceAndCapture is a test helper that calls CallResource and captures the response
 func callResourceAndCapture(t *testing.T, ds *Datasource, req *backend.CallResourceRequest) *backend.CallResourceResponse {
